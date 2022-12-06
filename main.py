f3c779a9e5c05a9fb317850e22284e67d4c702c9
Lazyferret <alex162.ru@yandex.ru>
2022-12-06 03:04:13 +0300

    Okay, it can scrap videos

--- main.py
+++ main.py
@@ -5,67 +5,66 @@ from selenium.webdriver.support.wait import WebDriverWait
 import json
 import time
 import sqlite3
+import itertools
+def human_type(element, text):
+    for char in text:
+        time.sleep(random.randint(1,10)/15) #fixed a . instead of a ,
+        element.send_keys(char)
+
 
 with open("logpass.txt", mode='r') as f:
     login, password = f.read().split()
 
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(executable_path=r'chromedriver.exe')
+try:
+    driver = webdriver.Chrome(executable_path=r'chromedriver.exe')
+except FileNotFoundError:
+    driver = webdriver.Chrome()
 driver.get("https://lms.termilab.ru/login")
 time.sleep(5)
 email_input = driver.find_element(By.ID, "email")
 email_input.clear()
-email_input.send_keys(login)
+human_type(email_input, login)
 time.sleep(3)
 password_input = driver.find_element(By.ID, "password")
 time.sleep(2)
 password_input.clear()
-password_input.send_keys(password)
+human_type(password_input, password)
 time.sleep(2)
 driver.find_element(By.ID, "submit").click()
 time.sleep(8)
 courses = driver.find_elements(By.CLASS_NAME, "course-item")
 data = {}
 for course in courses:
-    print(course.find_element(By.TAG_NAME,"h3").text)
-    """
+    course_name = course.find_element(By.TAG_NAME, "h3").text.strip()
+    print(course_name)
     course.find_element(By.CLASS_NAME, "enter-course").click()
     time.sleep(3)
     driver.find_element(By.CLASS_NAME, "course-tabs").find_element(By.TAG_NAME, "a").click()
     time.sleep(3)
-    chapters = driver.find_element(By.ID, "ui-accordion-accordion-header-1").find_elements(By.TAG_NAME, "li")
-    for chapter in chapters:
-        driver.get(chapter.find_element(By.TAG_NAME, "a").get_attribute("href"))
+    chapter_accordion = driver.find_element(By.ID, "ui-accordion-accordion-panel-1")
+    chapters = chapter_accordion.find_elements(By.TAG_NAME, "li")
+    chapters_hrefs = [[chapter.find_element(By.TAG_NAME, "a").get_attribute("href"),chapter.find_element(By.TAG_NAME, "a").text.strip()] for chapter in chapters]
+    for chapter, chapter_name in chapters_hrefs:
+        chapter_data = {}
+        driver.get(chapter)
         time.sleep(5)
-        headers = driver.find_elements(By.CLASS_NAME, "problem-header")
-        hrefs = driver.find_elements(By.CLASS_NAME, "link_lti_new_window")
-        #data[course.find_element(By.TAG_NAME,"h3").]
-
-    driver.close()
-    driver.quit()
-
-try:
-    for i in data.keys():
-        for j in data[i].keys():
-            url = data[i][j]
-            print(url)
-            driver.get(url)
-            time.sleep(2)
+        headers = [i.text.strip() for i in driver.find_elements(By.CLASS_NAME, "problem-header")]
+        hrefs = [i.get_attribute("href") for i in driver.find_elements(By.CLASS_NAME, "link_lti_new_window")]
+        for module in range(len(headers) - 1):
+            # chapter_data[headers[module].text.strip()] = hrefs[module].get_attribute("href")
+            driver.get(hrefs[module])
+            time.sleep(3)
             sources = driver.find_elements(By.TAG_NAME, "source")
             sources_urls = []
-            for k in sources:
-                sources_urls.append(k.get_attribute("src"))
-            data_back[i][j] = sources_urls
-            time.sleep(random.randint(1, 5))
-
-            # WebDriverWait(driver, timeout=50)
-
-except Exception as ex:
-    print("exception!", ex)
-finally:
-    with open("IT_Essentials_static.json", "w") as outfile:
-        json.dump(data_back, outfile, sort_keys=False, ensure_ascii=False)
+            for source in sources:
+                sources_urls.append(source.get_attribute("src"))
+            chapter_data[headers[module]] = sources_urls
+            time.sleep(random.randint(2, 6))
+        data[chapter_name] = chapter_data
+    driver.get("https://lms.termilab.ru/dashboard")
+driver.close()
+driver.quit()
+with open("IT_Essentials_static.json", "w") as outfile:
+    json.dump(data, outfile, sort_keys=False, ensure_ascii=False, indent=4)
     #    pass
-    driver.close()
-    driver.quit()"""
-
